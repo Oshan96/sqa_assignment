b0ca31fde1903576cd7e6fa191477756959d4a5c
iromiesamarasekara <2017is074@stu.ucsc.cmb.ac.lk>
2021-10-16 23:11:12 +0530

    Page Object Model commit

---
+++ sqaPOM/src/main/java/com/app/qa/pages/UsersPage.java
@@ -0,0 +1,91 @@
+package com.app.qa.pages;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.JavascriptExecutor;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.app.qa.base.TestBase;
+
+public class UsersPage extends TestBase{
+	
+	
+	
+	@FindBy(xpath = "//th[contains(@class,'userName firstLayer')]")
+	WebElement workLabel;
+	
+	@FindBy(xpath = "//*[contains(@class,'components_button withPlusIcon')]")
+	WebElement newUserLink;
+	
+	@FindBy(xpath = "//input[contains(@class,'firstNameField inputFieldWithPlaceholder')]")
+	WebElement firstName;
+	
+	@FindBy(name = "lastName")
+	WebElement lastName;
+	
+	@FindBy(name = "email")
+	WebElement Email;
+	
+	@FindBy(name = "Save & Send Invitation")
+	WebElement saveBtn;
+	
+	@FindBy(xpath = "//*[contains(@class,'title')]")
+	WebElement department;
+	
+	@FindBy(xpath = "//*[contains(@text,'Production')]")
+	WebElement prod;
+	
+	
+	// Initializing the Page Objects:
+	public UsersPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	public boolean verifyworkLabel(){
+		return workLabel.isDisplayed();
+	}
+	
+	
+	public void clickOnNewUserLink(){
+	
+		newUserLink.click();
+		
+	}
+	
+	
+	
+	public void createNewUser(String ftName, String ltName, String email){
+		
+		WebDriverWait wait = new WebDriverWait(driver, 60);
+		WebElement fNameEle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"createUserPanel_firstNameField\"]")));
+		fNameEle.sendKeys(ftName);
+		WebElement lNameEle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"createUserPanel_lastNameField\"]")));
+		lNameEle.sendKeys(ltName);
+        WebElement emailEle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"createUserPanel_emailField\"]")));
+        emailEle.sendKeys(email);
+        
+        WebElement save = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class=\"components_button_label\"]")));
+        save.click();
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@class,'firstNameField inputFieldWithPlaceholder')]")));
+		
+		
+		//WebDriverWait wait = new WebDriverWait(driver, 50);
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class,'title')]")));
+		//driver.findElement(By.xpath("//*[contains(@class,'title')]")).click();
+		//department.click();
+		//prod.click();
+		//saveBtn.click();
+		
+		
+	}
+	
+	
+
+}
